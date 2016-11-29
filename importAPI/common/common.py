86a3a96693defb8db3bd227206c2df936537086e
marin <marin>
2016-11-29 16:26:28 +0100

    Add sql link - Use ORM peewee - Described "station" table in sqlmodels.py - Added insert function in common.py

--- importAPI/common/common.py
+++ importAPI/common/common.py
@@ -1,3 +1,26 @@
+import sqlModels
+
+
+def insertvelibstation(values):
+    station = sqlModels.Station()
+    station.station_number = values['number']
+    station.station_name = values['contract_name']
+    station.contract_name = values['name']
+    station.address = values['address']
+    station.banking = values['banking']
+    station.bonus = values['bonus']
+    station.status = values['status']
+    station.operational_bike_stands = values['bike_stands']
+    station.available_bike_stands = values['available_bike_stands']
+    station.available_bikes = values['available_bikes']
+    station.last_update = values['last_update']
+    station.latitude = values['position']['lat']
+    station.longitude = values['position']['lng']
+    station.city_name = values['name']
+    station.country_code = 'FR'
+    station.save()
+
+
 # Travis test
 def doCommon(value):
     if value:
---
+++ importAPI/common/sqlModels.py
@@ -0,0 +1,37 @@
+import sqlCredentials
+from peewee import *
+
+# Constants
+DBname=sqlCredentials.DB
+DBuser=sqlCredentials.USER
+DBpassword=sqlCredentials.PASSWORD
+
+# Db connection
+db = MySQLDatabase(DBname, user=DBuser, passwd=DBpassword)
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Station(BaseModel):
+    id_station = PrimaryKeyField()
+    station_number = IntegerField()
+    station_name = CharField(255)
+    contract_name = CharField(255)
+    address = CharField(255)
+    banking = BooleanField()
+    bonus = BooleanField()
+    status = BooleanField()
+    operational_bike_stands = IntegerField()
+    available_bike_stands = IntegerField()
+    available_bikes = IntegerField()
+    last_update = BigIntegerField()
+    latitude = FloatField()
+    longitude = FloatField()
+    city_name = CharField(255)
+    country_code = CharField(255)
+
+# Create Tables if no exist
+db.create_tables([Station], safe=True)
